0836454f4c264d6bf404c375a1ea775f296765fd
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2021-08-05 21:37:28 +0100

    Move distributor init up

--- gensvm/__init__.py
+++ gensvm/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
+# Has to be first
+from . import _distributor_init
+
 from .__version__ import __version__
 
 from .core import GenSVM
 from .gridsearch import GenSVMGridSearchCV
-
-from . import _distributor_init
